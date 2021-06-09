14a66a14b1bea1c8a52aef434e84e071b64f94c9
orges <me@orgesified.ml>
2021-06-09 01:02:52 +0000

    soong: update to clang 12.0.6
    Signed-off-by: orges <me@orgesified.ml>

--- cc/config/global.go
+++ cc/config/global.go
@@ -137,8 +137,8 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r416183b"
-	ClangDefaultShortVersion = "12.0.5"
+	ClangDefaultVersion      = "clang-r416183c"
+	ClangDefaultShortVersion = "12.0.6"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
